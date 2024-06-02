fbaddb565cb7e1a2fb2abbb265537b65cb30e70c
chejen <jkopre.qek@gmail.com>
2024-06-02 21:56:38 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('GitHub Actions test', Date.now());
+console.log('GitHub Actions ENV - FOO', process.env.FOO);
+console.log('GitHub Actions ENV - BAR', process.env.BAR);
+console.log('GitHub Actions ENV - FOO2', process.env.FOO2);
