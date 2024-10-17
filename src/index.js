0954b131b9090996e57db0d3df58dde6d7235245
chejen <jkopre.qek@gmail.com>
2024-10-17 13:52:35 +0800

    test-2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 console.log('GitHub Actions ENV - FOO', process.env.FOO);
 console.log('GitHub Actions ENV - BAR', process.env.BAR);
 console.log('GitHub Actions ENV - FOO2', process.env.FOO2);
+console.log('test1', 3);
